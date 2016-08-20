7cc96248c2fcdd811518e618b5a70b86bc6a1e88
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-08-20 16:15:51 -0700

    adds page navigation and color scheme

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 $( document ).ready(function() {
-  console.log( "ready!" );
+ 
+  var $view1 = $('#view-1');
+  var $view2 = $('#view-2');
+  var $view3 = $('#view-3');
 
-  $('#view-1').show();
+  $view1.show();
+
+  var pageFlow = [$view1, $view2, $view3];
+  var counter = 0;
+
+  $('#prev').on('click', function(){
+  	if(counter > 0){
+  		pageFlow[counter].hide();
+  		counter--;
+  	  pageFlow[counter].show();
+  	}
+  });
+
+  $('#next').on('click', function(){
+  	if(counter < 2){
+  		pageFlow[counter].hide();
+  		counter++;
+  	  pageFlow[counter].show();
+  	}
+  });
+
 
 });
