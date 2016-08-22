644ea314b6091489d3262e89c947f6f65cbb7684
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-08-22 11:38:44 -0700

    adds dynamic flex-basis to view 2

--- view_2.js
+++ view_2.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
 
-  var buttonMap_1 = {
+  var viewMap_2 = {
     flex_basis: {
       status: false,
       options: ['', '30px', 'auto', 'fill']
@@ -16,10 +16,20 @@ $( document ).ready(function() {
   };
 
   $('#flex-basis').on('click', function(){
-    $('#css-2').html('<div><pre><code>#BOX_1{ flex-basis: 50px; } #BOX_2{ flex-basis: 100px; } #BOX3{ flex-basis: 150px; }</code></pre></div>');
-    $('#child-two-a').css('flex-basis', '50px');
-    $('#child-two-b').css('flex-basis', '100px');
-    $('#child-two-c').css('flex-basis', '150px');
+    if(viewMap_2['flex_basis']['status']){
+      viewMap_2['flex_basis']['status'] = false;
+      $('#css-2').html('<div><pre><code>#BOX_1{ flex-basis: 400px; } #BOX_2{ flex-basis: 400px; } #BOX3{ flex-basis: 400px; }</code></pre></div>');
+      $('#flex-basis').css('background-color', '#34495e');
+      $('#child-two-a').css('flex-basis', '400px');
+      $('#child-two-b').css('flex-basis', '400px');
+      $('#child-two-c').css('flex-basis', '400px');
+    } else {
+      viewMap_2['flex_basis']['status'] = true;
+      $('#css-2').html('<div><pre><code>#BOX_1{ flex-basis: 150px; } #BOX_2{ flex-basis: 250px; } #BOX3{ flex-basis: 350px; }</code></pre></div>');
+      $('#flex-basis').css('background-color', '#1abc9c');
+      $('#child-two-a').css('flex-basis', '150px');
+      $('#child-two-b').css('flex-basis', '250px');
+      $('#child-two-c').css('flex-basis', '350px');
+    }
   });
-
 });
