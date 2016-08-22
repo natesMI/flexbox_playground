ebf0ad84482d1081606f99f2693c60c28f620a38
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-08-21 18:06:22 -0700

    makes property toggle dynamic on view 1

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ $( document ).ready(function() {
   var pageFlow = [$view1, $view2, $view3];
   var counter = 0;
 
+  var buttonMap = {
+    display: false,
+    flex_direction: false,
+    justify_content: false,
+    align_items: false
+  };
+
   $('#prev').on('click', function(){
   	if(counter > 0){
   		pageFlow[counter].hide();
@@ -26,20 +33,49 @@ $( document ).ready(function() {
   });
 
   $('#display').on('click', function(){
-    $('#css-1').html('<pre><code>#purple-div { display: flex; }</code></pre>');
-    $('#container-one').css('display', 'flex');
+    if(buttonMap['display']){
+      buttonMap['display'] = false;
+      $('#css-1').html('<pre><code>#purple-div { display: block; }</code></pre>');
+      $('#container-one').css('display', 'block');
+    } else {
+      buttonMap['display'] = true;
+      $('#css-1').html('<pre><code>#purple-div { display: flex; }</code></pre>');
+      $('#container-one').css('display', 'flex');
+    }
   });
+
   $('#flex-direction').on('click', function(){
-    $('#css-1').html('<pre><code>#purple-div { display: flex; flex-direction: column; }</code></pre>');
-    $('#container-one').css('flex-direction', 'column');
+    if(buttonMap['display']){
+      if(buttonMap['flex_direction']){
+        buttonMap['flex_direction'] = false;
+        $('#css-1').html('<pre><code>#purple-div { display: flex; flex-direction: none; }</code></pre>');
+        $('#container-one').css('flex-direction', 'none');
+      } else {
+        buttonMap['flex_direction'] = true;
+        $('#css-1').html('<pre><code>#purple-div { display: flex; flex-direction: row; }</code></pre>');
+        $('#container-one').css('flex-direction', 'row');
+      }
+    } else {
+      $('#css-1').html('<h3>Must first toggle display property</h3>');
+    }
   });
+
   $('#justify-content').on('click', function(){
-    $('#css-1').html('<pre><code>#purple-div { display: flex; flex-direction: row; justify-content: center }</code></pre>');
-    $('#container-one').css('justify-content', 'center');
+    if(buttonMap['display']){
+      $('#css-1').html('<pre><code>#purple-div { display: flex; flex-direction: row; justify-content: center }</code></pre>');
+      $('#container-one').css('justify-content', 'center');
+    } else {
+      $('#css-1').html('<h3>Must first toggle display property</h3>');
+    }
   });
+
   $('#align-items').on('click', function(){
-    $('#css-1').html('<pre><code>#purple-div { display: flex; flex-direction: row; justify-content: center; align-items: center }</code></pre>');
-    $('#container-one').css('align-items', 'center');
+    if(buttonMap['display']){
+      $('#css-1').html('<pre><code>#purple-div { display: flex; flex-direction: row; justify-content: center; align-items: center }</code></pre>');
+      $('#container-one').css('align-items', 'center');
+    } else {
+      $('#css-1').html('<h3>Must first toggle display property</h3>');
+    }
   });
 
 });
