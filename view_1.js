c8ef2efe8d5aa2f145d36ad38db6e11a2734c1d4
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-08-23 19:42:06 -0700

    changes toggle buttons to individual buttons

--- view_1.js
+++ view_1.js
@@ -4,27 +4,25 @@ $( document ).ready(function() {
     display: false,
     flex_direction: {
       status: false,
-      options: ['', 'row', 'row-reverse', 'column', 'column-reverse']
+      options: ['', 'row', 'column', 'row-reverse', 'column-reverse']
     },
     justify_content: {
       status: false,
-      options: ['', 'flex-start', 'center', 'flex-end', 'space-around', 'space-between']
+      options: ['', 'flex-start', 'flex-end', 'center', 'space-around', 'space-between']
     },
     align_items: {
       status: false,
-      options: ['', 'flex-start', 'center', 'flex-end', 'space-around', 'space-between']
+      options: ['', 'flex-start', 'flex-end', 'center', 'baseline', 'stretch']
     }
   };
 
   $('#display').on('click', function(){
     if(viewMap_1['display']){
       viewMap_1['display'] = false;
-      $('#css-1').html('<h3>Purple Div CSS Properties</h3>');
       $('#display').css('background-color', '#34495e');
       $('#container-one').css('display', '');
     } else {
       viewMap_1['display'] = true;
-      $('#css-1').html('<div><pre><code>#purple-div{ display: flex; }</code></pre></div>');
       $('#display').css('background-color', '#1abc9c');
       $('#container-one').css('display', 'flex');
     }
@@ -34,12 +32,10 @@ $( document ).ready(function() {
     if(viewMap_1['display']){
       if(viewMap_1['flex_direction']['status']){
         viewMap_1['flex_direction']['status'] = false;
-        $('#css-1').html('<div><pre><code>#purple-div{ display: flex; }</code></pre></div>');
         $('#flex-direction').css('background-color', '#34495e');
         $('#container-one').css('flex-direction', '');
       } else {
         viewMap_1['flex_direction']['status'] = true;
-        $('#css-1').html('<div><pre><code>#purple-div{ display: flex; flex-direction: row; }</code></pre></div>');
         $('#flex-direction').css('background-color', '#1abc9c');
         $('#container-one').css('flex-direction', 'row');
       }
@@ -52,12 +48,10 @@ $( document ).ready(function() {
     if(viewMap_1['display']){
       if(viewMap_1['justify_content']['status']){
         viewMap_1['justify_content']['status'] = false;
-        $('#css-1').html('<div><pre><code> #purple-div{ display: flex; flex-direction: row; }</code></pre></div>');
         $('#justify-content').css('background-color', '#34495e');
         $('#container-one').css('justify-content', '');
       } else {
         viewMap_1['justify_content']['status'] = true;
-        $('#css-1').html('<div><pre><code>#purple-div{ display: flex; flex-direction: row; justify-content: center; }</code></pre></div>');
         $('#justify-content').css('background-color', '#1abc9c');
         $('#container-one').css('justify-content', 'center');
       }
@@ -70,12 +64,10 @@ $( document ).ready(function() {
     if(viewMap_1['display']){
       if(viewMap_1['align_items']['status']){
         viewMap_1['align_items']['status'] = false;
-        $('#css-1').html('<div><pre><code>#purple-div{ display: flex; flex-direction: row; justify-content: center; }</code></pre></div>');
         $('#align-items').css('background-color', '#34495e');
         $('#container-one').css('align-items', '');
       } else {
         viewMap_1['align_items']['status'] = true;
-        $('#css-1').html('<div><pre><code>#purple-div{ display: flex; flex-direction: row; justify-content: center; align-items: center; }</code></pre></div>');
         $('#align-items').css('background-color', '#1abc9c');
         $('#container-one').css('align-items', 'center');
       }
