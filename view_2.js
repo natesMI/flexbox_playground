fd590030b5aa6415a49bf41ba06de051a84b22aa
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-09-06 18:02:20 -0700

    converts button toggles to div children

--- view_2.js
+++ view_2.js
@@ -6,7 +6,7 @@ $( document ).ready(function() {
   };
   var flexGrow = {
     status: false,
-    value: ''
+    value: '',
   };
   var flexShrink = {
     status: false,
@@ -20,9 +20,9 @@ $( document ).ready(function() {
     flexBasis['status'] = true;
     flexBasis['value'] = '-small';
     $('#flex-basis-small').css('background-color', '#9b59b6');
-    $('#child-two-a').css('flex-basis', '200px');
-    $('#child-two-b').css('flex-basis', '200px');
-    $('#child-two-c').css('flex-basis', '200px');
+    $('#child-two-one').css('flex-basis', '200px');
+    $('#child-two-two').css('flex-basis', '200px');
+    $('#child-two-three').css('flex-basis', '200px');
   });
 
   $('#flex-basis-medium').on('click', function(){
@@ -32,9 +32,9 @@ $( document ).ready(function() {
     flexBasis['status'] = true;
     flexBasis['value'] = '-medium';
     $('#flex-basis-medium').css('background-color', '#9b59b6');
-    $('#child-two-a').css('flex-basis', '300px');
-    $('#child-two-b').css('flex-basis', '300px');
-    $('#child-two-c').css('flex-basis', '300px');
+    $('#child-two-one').css('flex-basis', '300px');
+    $('#child-two-two').css('flex-basis', '300px');
+    $('#child-two-three').css('flex-basis', '300px');
   });
 
   $('#flex-basis-large').on('click', function(){
@@ -44,39 +44,42 @@ $( document ).ready(function() {
     flexBasis['status'] = true;
     flexBasis['value'] = '-large';
     $('#flex-basis-large').css('background-color', '#9b59b6');
-    $('#child-two-a').css('flex-basis', '400px');
-    $('#child-two-b').css('flex-basis', '400px');
-    $('#child-two-c').css('flex-basis', '400px');
+    $('#child-two-one').css('flex-basis', '400px');
+    $('#child-two-two').css('flex-basis', '400px');
+    $('#child-two-three').css('flex-basis', '400px');
   });
 
-  $('#flex-grow-large').on('click', function(){
+  $('#flex-grow-one').on('click', function(){
     if(flexGrow['status']){
       $('#flex-grow' + flexGrow['value']).css('background-color', '#ffffff');
+      $('#child-one' + flexGrow['value']).css('flex-grow', '0');
     }
     flexGrow['status'] = true;
-    flexGrow['value'] = '-large';
-    $('#flex-grow-large').css('background-color', '#9b59b6');
-    $('#child-two-a').css('flex-grow', '3');
+    flexGrow['value'] = '-one';
+    $('#flex-grow-one').css('background-color', '#9b59b6');
+    $('#child-two-one').css('flex-grow', '1');
   });
 
-  $('#flex-grow-medium').on('click', function(){
+  $('#flex-grow-two').on('click', function(){
     if(flexGrow['status']){
       $('#flex-grow' + flexGrow['value']).css('background-color', '#ffffff');
+      $('#child-two' + flexGrow['value']).css('flex-grow', '0');
     }
     flexGrow['status'] = true;
-    flexGrow['value'] = '-medium';
-    $('#flex-grow-medium').css('background-color', '#9b59b6');
-    $('#child-two-b').css('flex-grow', '2');
+    flexGrow['value'] = '-two';
+    $('#flex-grow-two').css('background-color', '#9b59b6');
+    $('#child-two-two').css('flex-grow', '1');
   });
 
-  $('#flex-grow-small').on('click', function(){
+  $('#flex-grow-three').on('click', function(){
     if(flexGrow['status']){
       $('#flex-grow' + flexGrow['value']).css('background-color', '#ffffff');
+      $('#child-three' + flexGrow['value']).css('flex-grow', '0');
     }
     flexGrow['status'] = true;
-    flexGrow['value'] = '-small';
-    $('#flex-grow-small').css('background-color', '#9b59b6');
-    $('#child-two-c').css('flex-grow', '1');
+    flexGrow['value'] = '-three';
+    $('#flex-grow-three').css('background-color', '#9b59b6');
+    $('#child-two-three').css('flex-grow', '1');
   });
 
   $('#flex-shrink-small').on('click', function(){
@@ -86,7 +89,7 @@ $( document ).ready(function() {
     flexShrink['status'] = true;
     flexShrink['value'] = '-small';
     $('#flex-shrink-small').css('background-color', '#9b59b6');
-    $('#child-two-c').css('flex-shrink', '1');
+    $('#child-two-three').css('flex-shrink', '1');
   });
 
   $('#flex-shrink-medium').on('click', function(){
@@ -96,7 +99,7 @@ $( document ).ready(function() {
     flexShrink['status'] = true;
     flexShrink['value'] = '-medium';
     $('#flex-shrink-medium').css('background-color', '#9b59b6');
-    $('#child-two-b').css('flex-shrink', '2');
+    $('#child-two-two').css('flex-shrink', '2');
   });
 
   $('#flex-shrink-large').on('click', function(){
@@ -106,7 +109,7 @@ $( document ).ready(function() {
     flexShrink['status'] = true;
     flexShrink['value'] = '-large';
     $('#flex-shrink-large').css('background-color', '#9b59b6');
-    $('#child-two-a').css('flex-shrink', '3');
+    $('#child-two-one').css('flex-shrink', '3');
   });
 
 });
