2288a2a4c81912abe0fb87df4ff32f316534355d
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-09-08 07:40:02 -0700

    adds flex button toggle and ids for wrap items

--- view_3.js
+++ view_3.js
@@ -1,23 +1,34 @@
 $( document ).ready(function() {
 
-  var buttonMap_1 = {
-    flex: {
-      status: false,
-      options: ['', '1', '30px', 'auto']
-    },
-    order: {
-      status: false,
-      options: ['', '5', '-5']
-    },
-    flex_wrap: {
-      status: false,
-      options: ['', 'wrap', 'wrap-reverse', 'nowrap']
-    }
+  var flex = {
+    status: false,
+    value: ''
+  };
+  var order = {
+    status: false,
+    value: '',
+  };
+  var alignSelf = {
+    status: false,
+    value: ''
+  };
+  var flexWrap = {
+    status: false,
+    value: '',
+  };
+  var flexFlow = {
+    status: false,
+    value: '',
   };
 
-  $('#flex-basis').on('click', function(){
-    $('#css-3').html('<div><pre><code>flex-wrap: wrap;</code></pre></div>');
-    $('.wrap_item').css('flex-wrap', 'wrap');
-  });
+  $('#flex-one').on('click', function(){
+    if(flex['status']){
+      $('flex' + flex['value']).css('background-color', '#ffffff');
+    }
+    flex['status'] = true;
+    flex['value'] = '.uno';
+    $('.uno').css('flex', '1');
+    $('#flex-one').css('background-color', '#9b59b6');
+  })
 
 });
