50583a2b0f9858fa764748175c28b93e24300170
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-09-17 15:06:07 -0700

    deletes uneccesarry message modal

--- client/view_1.js
+++ client/view_1.js
@@ -35,14 +35,7 @@ $( document ).ready(function() {
       $('#container-one').css('flex-direction', 'row');
       flexDirection['status'] = true;
       flexDirection['value'] = '-row';
-    } else {
-      $('#message-display').html('<h1>*Must first select display property</h1>')
-        .show(1, function(){
-          setTimeout(function(){
-            $('#message-display').hide();
-          }, 2000);
-        });
-    }
+    } 
   });
 
   $('#flex-direction-column').on('click', function(){
@@ -54,14 +47,7 @@ $( document ).ready(function() {
       $('#container-one').css('flex-direction', 'column');
       flexDirection['status'] = true;
       flexDirection['value'] = '-column';
-    } else {
-      $('#message-display').html('<h1>*Must first select display property</h1>')
-        .show(1, function(){
-          setTimeout(function(){
-            $('#message-display').hide();
-          }, 2000);
-        });
-    }
+    } 
   });
 
   $('#flex-direction-row-reverse').on('click', function(){
@@ -73,14 +59,7 @@ $( document ).ready(function() {
       $('#container-one').css('flex-direction', 'row-reverse');
       flexDirection['status'] = true;
       flexDirection['value'] = '-row-reverse';
-    } else {
-      $('#message-display').html('<h1>*Must first select display property</h1>')
-        .show(1, function(){
-          setTimeout(function(){
-            $('#message-display').hide();
-          }, 2000);
-        });
-    }
+    } 
   });
 
   $('#flex-direction-column-reverse').on('click', function(){
@@ -92,14 +71,7 @@ $( document ).ready(function() {
       $('#container-one').css('flex-direction', 'column-reverse');
       flexDirection['status'] = true;
       flexDirection['value'] = '-column-reverse';
-    } else {
-      $('#message-display').html('<h1>*Must first select display property</h1>')
-        .show(1, function(){
-          setTimeout(function(){
-            $('#message-display').hide();
-          }, 2000);
-        });
-    }
+    } 
   });
 
   $('#justify-content-flex-start').on('click', function(){
@@ -111,14 +83,7 @@ $( document ).ready(function() {
       $('#container-one').css('justify-content', 'flex-start');
       justifyContent['status'] = true;
       justifyContent['value'] = '-flex-start';
-    } else {
-      $('#message-display').html('<h1>*Must first select display property</h1>')
-        .show(1, function(){
-          setTimeout(function(){
-            $('#message-display').hide();
-          }, 2000);
-        });
-    }
+    } 
   });
 
   $('#justify-content-flex-end').on('click', function(){
@@ -130,14 +95,7 @@ $( document ).ready(function() {
       $('#container-one').css('justify-content', 'flex-end');
       justifyContent['status'] = true;
       justifyContent['value'] = '-flex-end';
-    } else {
-      $('#message-display').html('<h1>*Must first select display property</h1>')
-        .show(1, function(){
-          setTimeout(function(){
-            $('#message-display').hide();
-          }, 2000);
-        });
-    }
+    } 
   });
 
   $('#justify-content-center').on('click', function(){
@@ -149,14 +107,7 @@ $( document ).ready(function() {
       $('#container-one').css('justify-content', 'center');
       justifyContent['status'] = true;
       justifyContent['value'] = '-center';
-    } else {
-      $('#message-display').html('<h1>*Must first select display property</h1>')
-        .show(1, function(){
-          setTimeout(function(){
-            $('#message-display').hide();
-          }, 2000);
-        });
-    }
+    } 
   });
 
   $('#justify-content-space-between').on('click', function(){
@@ -168,14 +119,7 @@ $( document ).ready(function() {
       $('#container-one').css('justify-content', 'space-between');
       justifyContent['status'] = true;
       justifyContent['value'] = '-space-between';
-    } else {
-      $('#message-display').html('<h1>*Must first select display property</h1>')
-        .show(1, function(){
-          setTimeout(function(){
-            $('#message-display').hide();
-          }, 2000);
-        });
-    }
+    } 
   });
 
   $('#justify-content-space-around').on('click', function(){
@@ -187,14 +131,7 @@ $( document ).ready(function() {
       $('#container-one').css('justify-content', 'space-around');
       justifyContent['status'] = true;
       justifyContent['value'] = '-space-around';
-    } else {
-      $('#message-display').html('<h1>*Must first select display property</h1>')
-        .show(1, function(){
-          setTimeout(function(){
-            $('#message-display').hide();
-          }, 2000);
-        });
-    }
+    } 
   });
 
   $('#align-items-flex-start').on('click', function(){
@@ -206,14 +143,7 @@ $( document ).ready(function() {
         $('#container-one').css('align-items', 'flex-start');
         alignItems['status'] = true;
         alignItems['value'] = '-flex-start';
-      } else {
-        $('#message-display').html('<h1>*Must first select display property</h1>')
-          .show(1, function(){
-            setTimeout(function(){
-              $('#message-display').hide();
-            }, 2000);
-          });
-      }
+      } 
   });
 
   $('#align-items-flex-end').on('click', function(){
@@ -225,14 +155,7 @@ $( document ).ready(function() {
         $('#container-one').css('align-items', 'flex-end');
         alignItems['status'] = true;
         alignItems['value'] = '-flex-end';
-      } else {
-        $('#message-display').html('<h1>*Must first select display property</h1>')
-          .show(1, function(){
-            setTimeout(function(){
-              $('#message-display').hide();
-            }, 2000);
-          });
-      }
+      } 
   });
 
   $('#align-items-center').on('click', function(){
@@ -244,14 +167,7 @@ $( document ).ready(function() {
         $('#container-one').css('align-items', 'center');
         alignItems['status'] = true;
         alignItems['value'] = '-center';
-      } else {
-        $('#message-display').html('<h1>*Must first select display property</h1>')
-          .show(1, function(){
-            setTimeout(function(){
-              $('#message-display').hide();
-            }, 2000);
-          });
-      }
+      } 
   });
 
   $('#align-items-baseline').on('click', function(){
@@ -263,13 +179,6 @@ $( document ).ready(function() {
         $('#container-one').css('align-items', 'baseline');
         alignItems['status'] = true;
         alignItems['value'] = '-baseline';
-      } else {
-        $('#message-display').html('<h1>*Must first select display property</h1>')
-          .show(1, function(){
-            setTimeout(function(){
-              $('#message-display').hide();
-            }, 2000);
-          });
       }
   });
 
@@ -282,14 +191,7 @@ $( document ).ready(function() {
         $('#container-one').css('align-items', 'stretch');
         alignItems['status'] = true;
         alignItems['value'] = '-stretch';
-      } else {
-        $('#message-display').html('<h1>*Must first select display property</h1>')
-          .show(1, function(){
-            setTimeout(function(){
-              $('#message-display').hide();
-            }, 2000);
-          });
-      }
+      } 
   });
 
 });
